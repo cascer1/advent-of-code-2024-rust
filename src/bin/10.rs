f12b47859b4f968e659324016db31b2d3a3f1794
Cas Eliëns <git@vobar.eu>
2025-12-18 16:08:10 +0100

    Begin day 10

---
+++ src/bin/10.rs
@@ -0,0 +1,32 @@
+advent_of_code::solution!(10);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(36));
+    }
+
+    #[test]
+    fn test_part_one_two() {
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
